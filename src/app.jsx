a612b2c549e2fdac2d5e0aaa36a1c1a3c2005bfe
Phu <phutrieu95@gmail.com>
2020-06-25 03:04:37 -0700

    Added search bar to search results list

--- src/app.jsx
+++ src/app.jsx
@@ -13,187 +13,187 @@ class App extends Component {
       homepageGif: null,
       searchBar: null,
       searchResults: [
-          {
-            "name": "TESLA INC",
-            "symbol": "TSLA",
-            "stock_exchange": {
-              "name": "NASDAQ Stock Exchange",
-              "acronym": "NASDAQ",
-              "mic": "XNAS",
-              "country": "USA",
-              "country_code": "US",
-              "city": "New York",
-              "website": "www.nasdaq.com",
-              "timezone": {
-                "timezone": "America/New_York",
-                "abbr": "EST",
-                "abbr_dst": "EDT"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC",
-            "symbol": "TSLA.XBUE",
-            "stock_exchange": {
-              "name": "Buenos Aires Stock Exchange",
-              "acronym": "BCBA",
-              "mic": "XBUE",
-              "country": "Argentina",
-              "country_code": "AR",
-              "city": "Buenos Aires",
-              "website": "www.bcba.sba.com.ar",
-              "timezone": {
-                "timezone": "America/Argentina/Buenos_Aires",
-                "abbr": "-03",
-                "abbr_dst": "-03"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC DRN",
-            "symbol": "TSLA34.BVMF",
-            "stock_exchange": {
-              "name": "B3 - Brasil Bolsa Balcão S.A",
-              "acronym": "Bovespa",
-              "mic": "BVMF",
-              "country": "Brazil",
-              "country_code": "BR",
-              "city": "Sao Paulo",
-              "website": "www.bmfbovespa.com.br",
-              "timezone": {
-                "timezone": "America/Sao_Paulo",
-                "abbr": "-03",
-                "abbr_dst": "-03"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC. DL -,001",
-            "symbol": "TL0.XFRA",
-            "stock_exchange": {
-              "name": "Deutsche Börse",
-              "acronym": "FSX",
-              "mic": "XFRA",
-              "country": "Germany",
-              "country_code": "DE",
-              "city": "Frankfurt",
-              "website": "www.deutsche-boerse.com",
-              "timezone": {
-                "timezone": "Europe/Berlin",
-                "abbr": "CET",
-                "abbr_dst": "CEST"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC. REGISTERED SHARES DL-,001",
-            "symbol": "TL0.XSTU",
-            "stock_exchange": {
-              "name": "Börse Stuttgart",
-              "acronym": "XSTU",
-              "mic": "XSTU",
-              "country": "Germany",
-              "country_code": "DE",
-              "city": "Stuttgart",
-              "website": "www.boerse-stuttgart.de",
-              "timezone": {
-                "timezone": "Europe/Berlin",
-                "abbr": "CET",
-                "abbr_dst": "CEST"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC. DL -,001",
-            "symbol": "TL0.XETRA",
-            "stock_exchange": {
-              "name": "Deutsche Börse Xetra",
-              "acronym": "XETR",
-              "mic": "XETRA",
-              "country": "Germany",
-              "country_code": "DE",
-              "city": "Frankfurt",
-              "website": "",
-              "timezone": {
-                "timezone": "Europe/Berlin",
-                "abbr": "CET",
-                "abbr_dst": "CEST"
-              }
-            }
-          },
-          {
-            "name": "TESLA",
-            "symbol": "TSLA.XMIL",
-            "stock_exchange": {
-              "name": "Borsa Italiana",
-              "acronym": "MIL",
-              "mic": "XMIL",
-              "country": "Italy",
-              "country_code": "IT",
-              "city": "Milano",
-              "website": "www.borsaitaliana.it",
-              "timezone": {
-                "timezone": "Europe/Rome",
-                "abbr": "CET",
-                "abbr_dst": "CEST"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC",
-            "symbol": "TSLA.XMEX",
-            "stock_exchange": {
-              "name": "Mexican Stock Exchange",
-              "acronym": "BMV",
-              "mic": "XMEX",
-              "country": "Mexico",
-              "country_code": "MX",
-              "city": "Mexico City",
-              "website": "www.bmv.com.mx",
-              "timezone": {
-                "timezone": "America/Mexico_City",
-                "abbr": "CST",
-                "abbr_dst": "CDT"
-              }
-            }
-          },
-          {
-            "name": "Tesla Exploration Ltd",
-            "symbol": "TXLZF",
-            "stock_exchange": {
-              "name": "OTC Grey Market",
-              "acronym": "OTCGREY",
-              "mic": "PSGM",
-              "country": "USA",
-              "country_code": "US",
-              "city": "New York",
-              "website": "www.otcmarkets.com",
-              "timezone": {
-                "timezone": "America/New_York",
-                "abbr": "EST",
-                "abbr_dst": "EDT"
-              }
-            }
-          },
-          {
-            "name": "TESLA INC",
-            "symbol": "TSLA",
-            "stock_exchange": {
-              "name": "Investors Exchange",
-              "acronym": "IEX",
-              "mic": "IEXG",
-              "country": "USA",
-              "country_code": "US",
-              "city": "New York",
-              "website": "www.iextrading.com",
-              "timezone": {
-                "timezone": "America/New_York",
-                "abbr": "EST",
-                "abbr_dst": "EDT"
-              }
+        {
+          "name": "TESLA INC",
+          "symbol": "TSLA",
+          "stock_exchange": {
+            "name": "NASDAQ Stock Exchange",
+            "acronym": "NASDAQ",
+            "mic": "XNAS",
+            "country": "USA",
+            "country_code": "US",
+            "city": "New York",
+            "website": "www.nasdaq.com",
+            "timezone": {
+              "timezone": "America/New_York",
+              "abbr": "EST",
+              "abbr_dst": "EDT"
             }
           }
-        ],
+        },
+        {
+          "name": "TESLA INC",
+          "symbol": "TSLA.XBUE",
+          "stock_exchange": {
+            "name": "Buenos Aires Stock Exchange",
+            "acronym": "BCBA",
+            "mic": "XBUE",
+            "country": "Argentina",
+            "country_code": "AR",
+            "city": "Buenos Aires",
+            "website": "www.bcba.sba.com.ar",
+            "timezone": {
+              "timezone": "America/Argentina/Buenos_Aires",
+              "abbr": "-03",
+              "abbr_dst": "-03"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC DRN",
+          "symbol": "TSLA34.BVMF",
+          "stock_exchange": {
+            "name": "B3 - Brasil Bolsa Balcão S.A",
+            "acronym": "Bovespa",
+            "mic": "BVMF",
+            "country": "Brazil",
+            "country_code": "BR",
+            "city": "Sao Paulo",
+            "website": "www.bmfbovespa.com.br",
+            "timezone": {
+              "timezone": "America/Sao_Paulo",
+              "abbr": "-03",
+              "abbr_dst": "-03"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC. DL -,001",
+          "symbol": "TL0.XFRA",
+          "stock_exchange": {
+            "name": "Deutsche Börse",
+            "acronym": "FSX",
+            "mic": "XFRA",
+            "country": "Germany",
+            "country_code": "DE",
+            "city": "Frankfurt",
+            "website": "www.deutsche-boerse.com",
+            "timezone": {
+              "timezone": "Europe/Berlin",
+              "abbr": "CET",
+              "abbr_dst": "CEST"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC. REGISTERED SHARES DL-,001",
+          "symbol": "TL0.XSTU",
+          "stock_exchange": {
+            "name": "Börse Stuttgart",
+            "acronym": "XSTU",
+            "mic": "XSTU",
+            "country": "Germany",
+            "country_code": "DE",
+            "city": "Stuttgart",
+            "website": "www.boerse-stuttgart.de",
+            "timezone": {
+              "timezone": "Europe/Berlin",
+              "abbr": "CET",
+              "abbr_dst": "CEST"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC. DL -,001",
+          "symbol": "TL0.XETRA",
+          "stock_exchange": {
+            "name": "Deutsche Börse Xetra",
+            "acronym": "XETR",
+            "mic": "XETRA",
+            "country": "Germany",
+            "country_code": "DE",
+            "city": "Frankfurt",
+            "website": "",
+            "timezone": {
+              "timezone": "Europe/Berlin",
+              "abbr": "CET",
+              "abbr_dst": "CEST"
+            }
+          }
+        },
+        {
+          "name": "TESLA",
+          "symbol": "TSLA.XMIL",
+          "stock_exchange": {
+            "name": "Borsa Italiana",
+            "acronym": "MIL",
+            "mic": "XMIL",
+            "country": "Italy",
+            "country_code": "IT",
+            "city": "Milano",
+            "website": "www.borsaitaliana.it",
+            "timezone": {
+              "timezone": "Europe/Rome",
+              "abbr": "CET",
+              "abbr_dst": "CEST"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC",
+          "symbol": "TSLA.XMEX",
+          "stock_exchange": {
+            "name": "Mexican Stock Exchange",
+            "acronym": "BMV",
+            "mic": "XMEX",
+            "country": "Mexico",
+            "country_code": "MX",
+            "city": "Mexico City",
+            "website": "www.bmv.com.mx",
+            "timezone": {
+              "timezone": "America/Mexico_City",
+              "abbr": "CST",
+              "abbr_dst": "CDT"
+            }
+          }
+        },
+        {
+          "name": "Tesla Exploration Ltd",
+          "symbol": "TXLZF",
+          "stock_exchange": {
+            "name": "OTC Grey Market",
+            "acronym": "OTCGREY",
+            "mic": "PSGM",
+            "country": "USA",
+            "country_code": "US",
+            "city": "New York",
+            "website": "www.otcmarkets.com",
+            "timezone": {
+              "timezone": "America/New_York",
+              "abbr": "EST",
+              "abbr_dst": "EDT"
+            }
+          }
+        },
+        {
+          "name": "TESLA INC",
+          "symbol": "TSLA",
+          "stock_exchange": {
+            "name": "Investors Exchange",
+            "acronym": "IEX",
+            "mic": "IEXG",
+            "country": "USA",
+            "country_code": "US",
+            "city": "New York",
+            "website": "www.iextrading.com",
+            "timezone": {
+              "timezone": "America/New_York",
+              "abbr": "EST",
+              "abbr_dst": "EDT"
+            }
+          }
+        }
+      ],
       stockDetails: null,
       stockDetailsGif: null
     }
@@ -245,7 +245,17 @@ class App extends Component {
       searchBar: searchQuery
     })
     fetch(`http://api.marketstack.com/v1/tickers?access_key=fb1fd1efa8b98380b5fee609590442a8&search=${searchQuery}`)
-      .then(res => res.json())
+      .then(res => {
+        // this.state.searchResults.length = 0;
+        if (this.state.searchResults.length !== 0) {
+          const parent = document.getElementById('search-results');
+          console.log(parent.firstChild)
+          while (parent.firstChild) {
+            parent.removeChild(parent.lastChild)
+          }
+        }
+        return res.json()
+      })
       .then(data => {
         this.setState({
           searchResults: data.data
@@ -255,7 +265,7 @@ class App extends Component {
 
   checkState() {
     if (this.state.stockDetails) return <StockDetails details={this.state.stockDetails} gif={this.state.stockDetailsGif} backToResults={this.backToResults} />
-    if (this.state.searchResults) return <SearchResultList searchResults={this.state.searchResults} stockDetails={this.stockDetails} />
+    if (this.state.searchResults.length !== 0) return <SearchResultList searchResults={this.state.searchResults} stockDetails={this.stockDetails} formSubmit={this.formSubmit} />
     if (this.state.homepageGif) {
       return <Homepage gif={this.state.homepageGif} formSubmit={this.formSubmit} />
     }
--- src/search-result-list.jsx
+++ src/search-result-list.jsx
@@ -4,7 +4,17 @@ import SearchResultListItem from './search-result-list-item';
 class SearchResultList extends Component {
   constructor(props) {
     super(props);
-    this.state = null
+    this.state = {
+      searchQuery: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange() {
+    const searchbar = document.getElementById('search2')
+    this.setState({
+      searchQuery: searchbar.value
+    })
   }
 
   render() {
@@ -14,11 +24,17 @@ class SearchResultList extends Component {
     )
     return (
       <div>
-        <form action="">
-
+        <form className="search-result-LI m-auto d-flex align-items-center" onSubmit={() => this.props.formSubmit(this.state.searchQuery)} >
+          <span className="fas fa-search search-symbol mr-2"></span>
+          <input
+            type="text"
+            className="w-25 text-body"
+            id="search2"
+            placeholder="e.g. SNAP"
+            onChange={this.handleInputChange} />
         </form>
         <div className="mobile-switch">
-          <div className="d-flex justify-content-between pt-3 search-result-LI m-auto">
+          <div className="d-flex justify-content-between pt-4 search-result-LI m-auto">
             <div className="d-flex search-result-symbol justify-content-between row">
               <h4 className="m-0 col-sm-4 d-flex align-items-center font-weight-bold">Symbol</h4>
               <h4 className="m-0 col-sm-8 d-flex align-items-center font-weight-bold">Company Name</h4>
@@ -28,20 +44,22 @@ class SearchResultList extends Component {
             </div>
           </div>
         </div>
-        <hr className="thicc-border hr-mobile-switch"/>
-        {
-          searchResults.map(searchResult => {
-            return (
-              <SearchResultListItem
-                key={searchResult.name}
-                name={searchResult.name}
-                symbol={searchResult.symbol}
-                stockEx={searchResult.stock_exchange.acronym}
-                stockDetails={this.props.stockDetails}
-              />
-            )
-          })
-        }
+        <hr className="thicc-border hr-mobile-switch" />
+        <div id="search-results">
+          {
+            searchResults.map(searchResult => {
+              return (
+                <SearchResultListItem
+                  key={searchResult.name}
+                  name={searchResult.name}
+                  symbol={searchResult.symbol}
+                  stockEx={searchResult.stock_exchange.acronym}
+                  stockDetails={this.props.stockDetails}
+                />
+              )
+            })
+          }
+        </div>
         <h3 className="text-center mt-5">End of results</h3>
       </div>
     )
